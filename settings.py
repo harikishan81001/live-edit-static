13c7a410eaf962f31ba0133af5ed01b6d1843c69
h@rikish@n <hrikishn@hari-Vostro-2420.(none)>
2013-03-18 18:25:32 +0530

    settings modified and codemirror editor added

--- settings.py
+++ settings.py
@@ -108,7 +108,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'Live_Edit.urls'
+ROOT_URLCONF = 'Live-Edit.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
